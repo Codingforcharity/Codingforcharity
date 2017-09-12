879d4a7ee157970b8af642ab5b3fadec8c9be7f2
Dylan Brown <dylandoesprogramming@gmail.com>
2017-09-12 12:27:30 -0500

    Got some auth0 working

--- index.js
+++ index.js
@@ -32,8 +32,52 @@ passport.use(new Auth0Strategy({
     clientSecret: process.env.clientSecret,
     callbackURL: '/auth/callback'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
-
+    const db = app.get('db');
+    console.log(profile);
+    db.getUserByAuthId([profile.id])
+        .then(
+            function(user) {
+                if (user.length < 1) {
+                    db.postUser(profile.id, profile.emails[0].value, profile.nickname, "I am a blank bio", "./DevGive-Blue-Logo.png", false).then(function(user) {
+                        profile.user = user[0];
+                        console.log(user);
+                        return done(null, profile);
+                    });
+                } else {
+                    profile.user = user[0];
+                    console.log(user[0]);
+                    return done(null, profile);
+                }
+            });
 }))
+
+app.get('/auth', passport.authenticate('auth0'))
+
+app.get('/auth/callback',
+    passport.authenticate('auth0', {
+        successRedirect: '/',
+        failureRedirect: '/auth'
+    })
+)
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+// Return user object from session
+app.get('/me', function(req, res) {
+    console.log(req.user.user)
+    res.send(req.user.user)
+})
+
+app.get('/auth/logout', function(req, res) {
+    req.logout();
+    res.redirect('https://dylandoesprogramming.auth0.com/v2/logout');
+})
 
 app.get("/api/charities", serverCtrl.getCharities);
 app.post("/api/postuser", serverCtrl.postUser);
