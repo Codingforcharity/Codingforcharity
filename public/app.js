8d29154c81880eeefc8689a1e437a35a87db5c81
Andy Parker <andyparker84@gmail.com>
2017-09-12 14:45:43 -0500

    Project feed created, added navbar links

--- public/app.js
+++ public/app.js
@@ -8,50 +8,53 @@ const app = angular.module("charityApp", ['ui.router'])
             })
             .state('accountDev', {
                 url: '/account/dev/:id',
-                templateUrl: '../public/views/accountDev/accountDev.html',
+                templateUrl: './views/accountDev/accountDev.html',
                 controller: 'accountDevCtrl',
             })
             .state('accountNonProfit', {
                 url: '/account/org/:id',
-                templateUrl: '../public/views/accountNonProfit/accountNonProfit.html',
+                templateUrl: './views/accountNonProfit/accountNonProfit.html',
                 controller: 'accountNonProfitCtrl',
             })
             .state('createAccount', {
                 url: '/sign-up',
-                templateUrl: '../public/views/createAccount/createAccount.html',
+                templateUrl: './views/createAccount/createAccount.html',
                 controller: 'createAccountCtrl',
             })
             .state('createProject', {
                 url: '/new-project',
-                templateUrl: '../public/views/createProject/createProject.html',
+                templateUrl: './views/createProject/createProject.html',
                 controller: 'createProjectCtrl',
             })
             .state('devProjectApplication', {
                 url: '/apply/:id',
-                templateUrl: '../public/views/devProjectApplication/devProjectApplication.html',
+                templateUrl: './views/devProjectApplication/devProjectApplication.html',
                 controller: 'devProjectApplicationCtrl',
             })
             .state('messenger', {
                 url: '/messenger',
-                templateUrl: '../public/views/messenger/messenger.html',
+                templateUrl: './views/messenger/messenger.html',
                 controller: 'messengerCtrl',
             })
             .state('projectFeed', {
                 url: '/projectfeed',
-                templateUrl: '../public/views//projectFeed/projectFeed.html',
+                templateUrl: './views/projectFeed/projectFeed.html',
                 controller: 'projectFeedCtrl',
             })
             .state('projectPublicDetails', {
                 url: '/projectfeed/project/:id',
-                templateUrl: '../public/views/devProjectApplication/devProjectApplication.html',
+                templateUrl: './views/devProjectApplication/devProjectApplication.htmldevProjectApplication.html',
                 controller: 'devProjectApplicationCtrl',
             })
             .state('workingProject', {
                 url: '/active/project/:id',
-                templateUrl: '../public/views/workingProject/workingProject.html',
+                templateUrl: './views/workingProject/workingProject.html',
                 controller: 'messengerCtrl',
             })
+            .state('login', {
+                url: '/login',
+                templateUrl: './views/login/login.html',
+                controller: 'loginCtrl' 
+              })
         $urlRouterProvider.otherwise('/')
-
-        // test
     })
