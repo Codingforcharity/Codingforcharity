4dd6aa53a1a763884168fd669f715da452d2c77e
Dylan Brown <dylandoesprogramming@gmail.com>
2017-09-13 15:37:16 -0500

    Project Feed is receiving data properly

--- public/views/projectFeed/projectFeedCtrl.js
+++ public/views/projectFeed/projectFeedCtrl.js
@@ -1,4 +1,23 @@
-app.controller('projectFeedCtrl', function($scope) {
+app.controller('projectFeedCtrl', function($scope, projectFeedSrvc) {
     console.log("projectFeedCtrl")
-    
+    $scope.curUser;
+    $scope.getProjects = () => {
+        projectFeedSrvc.getProjects()
+            .then((projects) => {
+                console.log(projects.data);
+                $scope.projects = [...projects.data]
+                console.log($scope.projects);
+            })
+    };
+
+    $scope.getLoggedUser = () => {
+        projectFeedSrvc.getLoggedUser()
+            .then((user) => {
+                $scope.curUser = Object.assign({}, user.data);
+                console.log($scope.curUser);
+                $scope.getProjects();
+            })
+    }
+
+    $scope.getLoggedUser();
 })
