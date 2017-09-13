8e643b5ae4d1e5ab815114d22aaec96bbc5421cc
Dylan Brown <dylandoesprogramming@gmail.com>
2017-09-13 12:10:34 -0500

    Delivered data to account page

--- public/views/accountDev/accountDevCtrl.js
+++ public/views/accountDev/accountDevCtrl.js
@@ -1,4 +1,26 @@
-app.controller('accountDevCtrl', function($scope) {
+app.controller('accountDevCtrl', function($scope, $stateParams, accountDevSrvc) {
     console.log("accountDev link")
-    
+    $scope.curUser;
+    $scope.ownPage = false;
+    $scope.getLoggedInUser = () => {
+        accountDevSrvc.getLoggedInUser()
+            .then((user) => {
+                if (!user.data.ischarity) {
+                    $scope.curUser = Object.assign({}, user.data);
+                    $scope.getUserByParams();
+                }
+            })
+    }
+    $scope.getUserByParams = () => {
+        const param = $stateParams.id;
+        if ($scope.curUser.id != param) {
+            accountDevSrvc.getUserByParams(param)
+                .then((user) => {
+                    console.log(user.data);
+                })
+        } else {
+            $scope.ownPage = true;
+        }
+    }
+    $scope.getLoggedInUser();
 })
