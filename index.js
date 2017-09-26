fe67d49329d3b4bbc8394c1e78e68b9d73ba1a33
Dylan Brown <dylandoesprogramming@gmail.com>
2017-09-26 12:50:57 -0500

    Fixed logout

--- index.js
+++ index.js
@@ -93,7 +93,8 @@ app.get('/me', function(req, res) {
 
 app.get('/auth/logout', function(req, res) {
     req.logout();
-    res.redirect('https://dylandoesprogramming.auth0.com/v2/logout?returnTo=' + req.query.fullUrl);
+    // res.redirect('https://dylandoesprogramming.auth0.com/v2/logout?returnTo=' + req.query.fullUrl);
+    res.redirect('https://dylandoesprogramming.auth0.com/v2/logout?returnTo=' + 'http://www.devgive.net/#!/');
 })
 
 app.get("/api/charities", serverCtrl.getCharities);
